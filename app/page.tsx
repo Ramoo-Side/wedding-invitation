60e0ed2cfc1ab13933adf2c319b5573f3124fc89
hws522 <sraphic@kakao.com>
2022-12-03 23:59:09 +0900

    nextjs12 => 13

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+                2022년 4월 9일 오후 6시
+            </h1>
+            <br />
+            <h1 className={styles.title}>
+                결혼합니다👰🤵
+            </h1>
+        </main>
+    )
+}
