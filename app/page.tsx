caa86fb57f1666f84263b4bd626fb5ce1292c097
hws522 <sraphic@kakao.com>
2022-12-12 23:22:22 +0900

    초대말 추가

--- app/page.tsx
+++ app/page.tsx
@@ -28,18 +28,80 @@ export default function Page() {
           <div>{'2022년 4월 9일 토요일 오후 6시'}</div>
           <div>{'팜파스 13 (구 더 자스민 하우스)'}</div>
         </StyleDateWrapper>
-        {/* <div>인사말 시 삽입</div>
-        <div>초대말</div>
-        <div>작은 가로 사진</div>
-        <div>누구의 아들 누구</div>
-        <div>누구의 딸 누구</div>
-        <div>혼주 연락처 버튼 모달</div>
-        <div>날짜 달력</div>
-        <div>가로 캐러우쉘 갤러리</div>
-        <div>오시는길 지도</div>
-        <div>코로나 인사말</div>
-        <div>마음 전하실 곳</div>
-        <div>카카오톡으로 공유하기</div> */}
+        <StyleGreeting>
+          {/* <img src="/flower.png" alt="flower" /> */}
+          <div
+            style={{
+              padding: '6.25rem 0',
+              whiteSpace: 'pre-wrap',
+              lineHeight: '2rem',
+              fontSize: '1.125rem',
+            }}
+          >
+            두 사람이 꽃과 나무처럼 걸어와서
+            <br />
+            서로의 모든 것이 되기 위해
+            <br />
+            오랜 기다림 끝에 혼례식을 치르는 날
+            <br />
+            세상은 더욱 아름다워라
+            <br />
+            <br />
+            이해인, &lt;사랑의 사람들이여&gt;
+          </div>
+          <div
+            style={{
+              padding: '3.125rem 0',
+              whiteSpace: 'pre-wrap',
+              lineHeight: '2rem',
+              fontSize: '1.125rem',
+            }}
+          >
+            <span
+              style={{
+                fontSize: `1.625rem`,
+                color: '#f1965f',
+                fontWeight: 'bold',
+                letterSpacing: '0.3125rem',
+              }}
+            >
+              초대합니다
+            </span>
+            <br />
+            <div style={{ padding: '2rem 1rem', lineHeight: '2.5rem', letterSpacing: '0.125rem' }}>
+              <div>
+                살랑이는 바람결에
+                <br />
+                사랑이 묻어나는 계절입니다.
+                <br />
+                여기 곱고 예쁜 두 사람이
+                <span>
+                  <span style={{ color: '#f1965f', fontWeight: 'bold' }}>사랑</span>
+                </span>
+                을 맺어
+                <br />
+                인생의 반려자가 되려 합니다.
+                <br />
+                새 인생을 시작하는 이 자리에 오셔서
+                <br />
+                <span>
+                  <span style={{ color: '#f1965f', fontWeight: 'bold' }}>축복</span>
+                </span>
+                해 주시면 감사하겠습니다.
+              </div>
+            </div>
+          </div>
+        </StyleGreeting>
+        {/* <div>작은 가로 사진</div> */}
+        {/* <div>누구의 아들 누구</div> */}
+        {/* <div>누구의 딸 누구</div> */}
+        {/* <div>혼주 연락처 버튼 모달</div> */}
+        {/* <div>날짜 달력</div> */}
+        {/* <div>가로 캐러우쉘 갤러리</div> */}
+        {/* <div>오시는길 지도</div> */}
+        {/* <div>코로나 인사말</div> */}
+        {/* <div>마음 전하실 곳</div> */}
+        {/* <div>카카오톡으로 공유하기</div> */}
       </StyleContentWrapper>
     </>
   );
@@ -119,3 +181,5 @@ const StylePhotoWrapper = styled.div`
   position: relative;
   margin-top: -1.5625rem;
 `;
+
+const StyleGreeting = styled.div``;
