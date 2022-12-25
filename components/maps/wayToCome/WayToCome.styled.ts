a4b41e033a0a310055dc71c03494a036c11979ac
hws522 <sraphic@kakao.com>
2022-12-25 22:57:22 +0900

    waytocome component styledcomponent 추가

--- components/maps/wayToCome/WayToCome.styled.ts
+++ components/maps/wayToCome/WayToCome.styled.ts
@@ -3,4 +3,18 @@ import styled from 'styled-components';
 export const StyleWayToComeWrapper = styled.div(() => ({
   padding: '0 2rem',
   textAlign: 'left',
+  marginTop: 20,
+}));
+
+export const StyleTitle = styled.div(() => ({
+  fontSize: `1.2rem`,
+  color: `#f1965f`,
+  fontWeight: '500',
+  letterSpacing: 1.5,
+  marginBottom: 10,
+}));
+
+export const StyleContent = styled.div(() => ({
+  lineHeight: '1.7rem',
+  color: '#333',
 }));
--- components/maps/wayToCome/WayToCome.tsx
+++ components/maps/wayToCome/WayToCome.tsx
@@ -1,37 +1,27 @@
 import React from 'react';
-import { StyleWayToComeWrapper } from './WayToCome.styled';
+import { StyleContent, StyleTitle, StyleWayToComeWrapper } from './WayToCome.styled';
 
 const WayToCome = () => {
   return (
     <StyleWayToComeWrapper>
-      <div style={{ marginTop: 20 }}>
-        <div
-          style={{
-            fontSize: `1.2rem`,
-            color: `#f1965f`,
-            fontWeight: '500',
-            letterSpacing: 1.5,
-            marginBottom: 10,
-          }}
-        >
-          <span>버스안내</span>
+      <StyleTitle>
+        <span>버스안내</span>
+      </StyleTitle>
+      <StyleContent>
+        <div>
+          {`[ KTX 울산역 기준 ]`}
+          <br />
+          {`5001 -> 401 명촌정문앞 하차`}
+          <br />
+          {`5003 -> 947 명촌동 하차`}
+          <br />
+          {`5004 -> 427 명촌동 하차`}
+          <p></p>
+          {`[ 태화강역 기준 ]`}
+          <br />
+          {`107, 127, 401, 411, 427`}
         </div>
-        <div style={{ lineHeight: '1.7rem', color: '#333' }}>
-          <div>
-            {`[ KTX 울산역 기준 ]`}
-            <br />
-            {`5001 -> 401 명촌정문앞 하차`}
-            <br />
-            {`5003 -> 947 명촌동 하차`}
-            <br />
-            {`5004 -> 427 명촌동 하차`}
-            <p></p>
-            {`[ 태화강역 기준 ]`}
-            <br />
-            {`107, 127, 401, 411, 427`}
-          </div>
-        </div>
-      </div>
+      </StyleContent>
     </StyleWayToComeWrapper>
   );
 };
