6d27e84de3241d109b3f2742388c86fe6652bd1a
hws522 <sraphic@kakao.com>
2022-12-31 23:37:20 +0900

    공유 버튼 css 수정

--- components/shareOnKakao/ShareOnKakao.tsx
+++ components/shareOnKakao/ShareOnKakao.tsx
@@ -18,7 +18,47 @@ const ShareOnKakao = () => {
     window.Kakao.Share.sendDefault(options);
   };
 
-  return <button onClick={shareOnKakaoFunc}>공유하기</button>;
+  return (
+    <>
+      <div
+        style={{
+          marginTop: '60px',
+          padding: '30px 0',
+          background: '#f1965f12',
+          lineHeight: '2rem',
+        }}
+      >
+        <div
+          onClick={shareOnKakaoFunc}
+          style={{
+            margin: '0px auto',
+            width: '300px',
+            cursor: 'pointer',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <img src="kakaotalk-white-logo.png" style={{ width: 30, height: 30 }} />
+          <span style={{ width: '11rem', marginLeft: 10 }}>카카오톡으로 공유하기</span>
+        </div>
+        <div
+          // onClick={shareOnKakaoFunc}
+          style={{
+            margin: '0px auto',
+            width: '300px',
+            cursor: 'pointer',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <img src="link.png" style={{ width: 20, height: 20 }} />
+          <span style={{ width: '11rem', marginLeft: 10 }}>청첩장 주소 복사하기</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ShareOnKakao;
