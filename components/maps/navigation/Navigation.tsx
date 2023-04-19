d11c5da446c598809fafa947fd12aabaa251376e
hws522 <sraphic@kakao.com>
2023-04-19 18:50:55 +0900

    네비게이션 모바일에서만 선택되도록 조건 처리, sweetAlert 추가, sweetAlert modal background 에서 생기는 padding 무시

--- components/maps/navigation/Navigation.tsx
+++ components/maps/navigation/Navigation.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 import { IProps } from '../kakaoMap/KakaoMap';
 import {
   StyleKakaoWrapper,
@@ -12,7 +13,20 @@ const NAVER_LOGO = '/navermap-icon.png';
 const TMAP_LOGO = '/tmap-icon.png';
 
 const Navigation = ({ locationInfo }: IProps) => {
-  const kakaoNavigation = () => {
+  const pushAlert = () => {
+    Swal.fire({
+      title: `<span style='font-size: 20px'>모바일에서만 확인 가능합니다</span>`,
+      showConfirmButton: false,
+      width: `20rem`,
+      // timer: 1000,
+    });
+  };
+
+  const kakaoNavigation = (e: { preventDefault: () => void }) => {
+    if (!/Mobi/i.test(window.navigator.userAgent)) {
+      e.preventDefault();
+      pushAlert();
+    }
     window.Kakao.Navi.start({
       name: locationInfo.title,
       x: locationInfo.coord[0],
@@ -29,19 +43,35 @@ const Navigation = ({ locationInfo }: IProps) => {
             href={`nmap://search?query=${encodeURIComponent(locationInfo.title)}&amp;appname=${
               locationInfo.appURL
             }`}
+            onClick={(e) => {
+              if (!/Mobi/i.test(window.navigator.userAgent)) {
+                e.preventDefault();
+                pushAlert();
+              }
+            }}
+            style={{ textDecoration: 'none', color: '#333' }}
           >
             <StyleNaviLogo src={NAVER_LOGO} />
             <span>네이버 지도</span>
           </a>
         </StyleNaverWrapper>
         <StyleKakaoWrapper>
-          <a href="#" onClick={kakaoNavigation}>
+          <a href="#" onClick={kakaoNavigation} style={{ textDecoration: 'none', color: '#333' }}>
             <StyleNaviLogo src={KAKAO_LOGO} />
             <span>카카오 네비</span>
           </a>
         </StyleKakaoWrapper>
         <StyleTmapWrapper>
-          <a href={`tmap://search?name=${locationInfo.title}`}>
+          <a
+            href={`tmap://search?name=${locationInfo.title}`}
+            onClick={(e) => {
+              if (!/Mobi/i.test(window.navigator.userAgent)) {
+                e.preventDefault();
+                pushAlert();
+              }
+            }}
+            style={{ textDecoration: 'none', color: '#333' }}
+          >
             <StyleNaviLogo src={TMAP_LOGO} />
             <span>티맵</span>
           </a>
